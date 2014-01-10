c4e49bfb50a5ff1b29972150c36267143f57781e
Michael Johnson <mj82@129-79-9-75.dhcp-bl.indiana.edu>
2014-01-10 16:23:16 -0500

    Add mutt and some supporting emacs files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="emacs tmux.conf emacs.d"    # list of files/folders to symlink in homedir
+files="emacs tmux.conf emacs.d muttrc mutt"    # list of files/folders to symlink in homedir
 
 ##########
 
